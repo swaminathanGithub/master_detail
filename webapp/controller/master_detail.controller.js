9e5ab3954829534973f304a57167c460d0076fac
Thangaraj Swaminathan <tswami@icloud.com>
2025-12-27 00:43:59 +0000

    Intial order is taken

--- webapp/controller/master_detail.controller.js
+++ webapp/controller/master_detail.controller.js
@@ -3,9 +3,9 @@ sap.ui.define([
   "sap/ui/model/Filter",
   "sap/ui/model/FilterOperator",
   "sap/ui/model/FilterType",
-  "sap/ui/model/json/JSONModel",  
+  "sap/ui/model/json/JSONModel",
 
-], (Controller, Filter, FilterOperator, FilterType,JSONModel) => {
+], (Controller, Filter, FilterOperator, FilterType, JSONModel) => {
   "use strict";
 
   return Controller.extend("masterdetail.masterdetail.controller.master_detail", {
@@ -26,22 +26,62 @@ sap.ui.define([
       var productID = oEvent.getSource().getBindingContext().getProperty("ProductID")
       var oModel = this.getOwnerComponent().getModel();
       oModel.read("/Products(" + productID + ")", {
-          success: function (oData) {
-            var jData = new JSONModel(oData);
-            that.getView().byId("productForm").setModel(jData);
-            that.getSplitContObj().to(that.createId("productDetailPage"))
-          }, error: function (oError) {            
-            console.log(oError);
-          }
-        })
+        success: function (oData) {
+          var jData = new JSONModel(oData);
+          that.getView().byId("productForm").setModel(jData);
+          that.getSplitContObj().to(that.createId("productDetailPage"))
+        }, error: function (oError) {
+          console.log(oError);
+        }
+      })
     },
 
-    getSplitContObj: function(){
+    getSplitContObj: function () {
       var result = this.byId("SplitCont");
       return result;
     },
-    onProductBack: function(){
+    onProductBack: function () {
       this.getSplitContObj().backDetail();
+    },
+
+    onUpdateStarted: function (oEvent) {
+      //onPressOrderdetail(oEvent);
+
+      var that = this;
+      //var productID = oEvent.getSource().getBindingContext().getProperty("ProductID")
+      // var oModel = this.getOwnerComponent().getModel();
+      // oModel.read("/Products(" + productID + ")", {
+      //     success: function (oData) {
+      //       var jData = new JSONModel(oData);
+      //       that.getView().byId("productForm").setModel(jData);
+      //       that.getSplitContObj().to(that.createId("productDetailPage"))
+      //     }, error: function (oError) {            
+      //       console.log(oError);
+      //     }
+      //   })
+
+
+    },
+    onUpdateFinished: function (oEvent) {
+      sap.ui.core.BusyIndicator.hide();
+      var oBinding = oEvent.getSource().getBinding("items");
+      if (oBinding) {
+        var aContexts = oBinding.getCurrentContexts();
+
+        if (aContexts.length > 0) {
+          var OrderID = aContexts[0].getProperty("OrderID");
+          console.log("First Order ID:", OrderID);
+        }
+      }     
+      var oFilter = new Filter("OrderID", FilterOperator.EQ, OrderID);
+       this.byId("orderTable").getBinding("items").filter([oFilter], FilterType.Application);
+       //this.getSplitContObj().to(this.createId("orderDetail"));            
+
+       this.getOwnerComponent().getModel().attachBatchRequestCompleted(() => 
+        {
+          this.byId("orderTable").setBusy(false);
+        });
+
     }
 
   });
